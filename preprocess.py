0563e824107ffe4152ea2d7759399c233be61ee6
Davide Stenner <davide.stenner@cefriel.com>
2023-10-05 15:51:51 +0200

    argument

--- preprocess.py
+++ preprocess.py
@@ -7,6 +7,8 @@ if __name__ == '__main__':
     
     parser.add_argument('--dev', action='store_true')
     parser.add_argument('--dash_data', action='store_true')
+    parser.add_argument('--filter_train', action='store_false')
+
     args = parser.parse_args()
         
-    train_pipeline(dev=args.dev, dash_data=args.dash_data)
+    train_pipeline(filter_train=args.filter_train, dev=args.dev, dash_data=args.dash_data)

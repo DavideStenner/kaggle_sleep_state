36c3e648f683addb04d3d7b3cdf75199f4e11fff
Davide Stenner <davide.stenner@cefriel.com>
2023-10-14 20:30:14 +0200

    minor fix

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,12 @@ from typing import Dict, Any
 def import_config_dict(config_path: str='config/config.json') -> Dict[str, Any]:
     with open(config_path) as config_file:
         config = json.load(config_file)
+    
+    config['TOLERANCES'] = {
+        int(key_): value_
+        for key_, value_ in config['TOLERANCES'].items()
+    }
+
     return config
 
 def import_params(model_name: str, config_path: str='config/model') -> Dict[str, Any]:
